86d630a4b58bbe136e029d0b2de85c19802e9def
Ariel Szabó <arielszabo@outlook.com>
2022-03-25 23:24:56 +0100

    create hokify navbar

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <body class="bg-transparent">
+    <div
+      class="w-1/2 m-6 p-6 bg-purple-100 rounded-2xl shadow-md border-purple-900 border-dashed"
+    >
+      <h1 class="text-3xl font-bold">Hello!</h1>
+      <p class="text-lg font-medium leading-6 text-gray-800">
+        My name is Ariel
+      </p>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body class="bg-transparent">
+      <!-- Logo -->
+      <header class="flex items-center w-full top-0 z-10 h-[1.4rem] p-[3.2rem]">
+        <img
+          src="https://tse3.mm.bing.net/th?id=OIP.jgsmLtRtQlxdbXz1foK9rwHaCs&pid=Api&P=0&w=518&h=189"
+          alt="Hokify Logo"
+          width="135"
+        />
+        <!-- Nav Links -->
+
+        <!-- External(Ext) Links -->
+        <div class="flex justify-between w-full m-10">
+          <div>
+            <a
+              href="#jobsuche"
+              class="text-3xl font-semibold text-white px-[0.8rem] bg-transparent hover:border-b-4 border-solid m-10"
+              >Jobsuche</a
+            >
+            <a
+              href="#jobsuche"
+              class="text-3xl font-semibold text-white px-[0.8rem] py-[0.4rem] bg-transparent hover:border-b-4 border-solid m-10"
+              >Lebenslauf</a
+            >
+            <a
+              href="#jobsuche"
+              class="text-3xl font-semibold text-white px-[0.8rem] py-[0.4rem] bg-transparent hover:border-b-4 border-solid m-10"
+              >Karriere-Tipps</a
+            >
+            <a
+              href="#jobsuche"
+              class="text-3xl font-semibold text-white px-[0.8rem] py-[0.4rem] bg-transparent hover:border-b-4 border-solid m-10"
+              >Für Arbeitgeber</a
+            >
+          </div>
+        </div>
+        <div class="mr-10">
+          <a
+            href="#jobsuche"
+            class="text-3xl font-semibold text-white px-[0.8rem] py-[0.4rem] bg-transparent hover:border-b-4 border-solid"
+            >Login</a
+          >
+        </div>
+      </header>
+      <div class="flex">
+        <div class="left-container">aaa</div>
+        <div class="right-container"></div>
+      </div>
+    </body>
+  </html>
+</template>
+
+<!--<button
+        class="bg-blue-200 hover:bg-green-200 text-black px-4 py-2 m-6 rounded-md text-xl font-semibod transition-all ease-in-out duration-300 sm:bg-yellow-500 sm:text-white md:bg-purple-400 dark:bg-gray-300 dark:text-white"
+      >
+        Save changes
+      </button>-->
